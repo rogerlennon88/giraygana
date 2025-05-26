4e711fc5cfb3cd0ee0d9a3640dc1ce1b6f201397
Roger Zea <rogerzea.dm@gmail.com>
2025-05-26 15:42:09 -0500

    Mejoras en el aspecto visual y comportamiento del formulario del lado del cliente

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* js/main.js */
 
 const spinButton = document.getElementById("spin-button")
-const finalizarButton = document.getElementById("finalizar-button")
+const finalizeButton = document.getElementById("finalizar-button")
 const modal = document.getElementById("modal")
 const modalText = document.getElementById("modal-text")
 const prizeWheel = document.querySelector(".spin--wheel")
@@ -12,19 +12,71 @@ const sectors = {
   s2: { label: "Pierdes", type: "loss", rotation: 330, area: [305, 355] },
   s3: { label: "50K", type: "prize", rotation: 270, area: [245, 295] },
   s4: { label: "Otro Intento", type: "joker", rotation: 210, area: [185, 235] },
-  s5: { label: "30K", type: "prize", rotation: 150, area: [125, 175] },
-  s6: { label: "Pierdes", type: "loss", rotation: 90, area: [65, 115] },
+  s5: { label: "30K", type: "prize", rotation: 270, area: [245, 295] },
+  s6: { label: "Pierdes", type: "loss", rotation: 330, area: [305, 355] },
+}
+
+const documentValidationPatterns = {
+  cc: {
+    pattern: /^\d{7,10}$/,
+    errorMessage: "La Cédula de Ciudadanía debe ser un número de 7 a 10 dígitos.",
+  },
+  ce: {
+    pattern: /^\d{6,10}$/,
+    errorMessage: "La Cédula de Extranjería debe ser un número de 6 a 10 dígitos.",
+  },
+  pa: {
+    pattern: /^[a-zA-Z0-9]{6,10}$/,
+    errorMessage: "El Pasaporte debe ser alfanumérico y tener entre 6 y 10 caracteres.",
+  },
 }
 
 let spinResult
 let spinning = false
 
 const joker = 3
-let prize = parseInt(prompt("ingresa un indice"))
-
+// let prize = parseInt(prompt("ingresa un indice"))
+let prize = 1
 let score = 0
 
 document.addEventListener("DOMContentLoaded", () => {
+  // --- Helper functions ---
+  const getFinalRotationPoint = (selectedArea) => {
+    const minAngle = selectedArea[0]
+    const maxAngle = selectedArea[1]
+    const possibleAngles = []
+    for (let angle = minAngle; angle <= maxAngle; angle += 5) {
+      possibleAngles.push(angle)
+    }
+
+    const randomIndex = Math.floor(Math.random() * possibleAngles.length)
+    return possibleAngles[randomIndex]
+  }
+
+  const toggleModal = (show) => {
+    if (show) {
+      modal.classList.remove("hidden")
+    } else {
+      modal.classList.add("hidden")
+    }
+  }
+
+  const messagePrizeWheel = (sector) => {
+    if (sector.type === "prize") {
+      console.log("spinPrizeWheel - type: prize, label:", sector.label)
+      const prizeText = `¡Felicitaciones, ganaste ${sector.label}!`
+      modalText.textContent = prizeText
+      finalizeButton.style.display = "block"
+    } else if (sector.type === "loss") {
+      console.log("spinPrizeWheel - type: loss, label:", sector.label)
+      modalText.textContent = "¡Pierdes! Inténtalo de nuevo."
+      finalizeButton.style.display = "block"
+    } else {
+      modalText.textContent = ""
+      finalizeButton.style.display = "none"
+    }
+  }
+
   // Function to define the spin result
   function defineSpinResult() {
     console.log("defineSpinResult - prize:", prize)
@@ -53,24 +105,24 @@ document.addEventListener("DOMContentLoaded", () => {
     spinning = true
     spinButton.disabled = true
 
-    spinResult = defineSpinResult();
-    console.log("spinPrizeWheel - spinResult:", spinResult);
+    spinResult = defineSpinResult()
+    console.log("spinPrizeWheel - spinResult:", spinResult)
 
-    let selectedArea = sectors[spinResult].area;
-    let rotation = getFinalRotationPoint(selectedArea);
-    console.log(rotation);
+    let selectedArea = sectors[spinResult].area
+    let rotation = getFinalRotationPoint(selectedArea)
+    console.log(rotation)
 
-    const animationDuration = 3000 // 10 seconds
+    const animationDuration = 6000 // 10 seconds
     prizeWheel.style.transition = `transform ${animationDuration}ms cubic-bezier(0.25, 0.1, 0.25, 1)`
     prizeWheel.style.transform = `rotate(${3600 + rotation}deg)`
 
     setTimeout(() => {
-      prizeWheel.style.transition = "none";
-      prizeWheel.style.transform = `rotate(${rotation}deg)`;
-      spinning = false;
+      prizeWheel.style.transition = "none"
+      prizeWheel.style.transform = `rotate(${rotation}deg)`
+      spinning = false
 
-      displayModal(sectors[spinResult]);
-    }, animationDuration);
+      displayModal(sectors[spinResult])
+    }, animationDuration)
   }
 
   // Function to hide the modal
@@ -80,63 +132,58 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Event listeners
   spinButton.addEventListener("click", spinPrizeWheel)
-  finalizarButton.addEventListener("click", hideModal)
+  finalizeButton.addEventListener("click", hideModal)
 
-  spinButton.disabled = false
+  function setupRegisterForm() {
+    const documentTypeSelect = document.getElementById("document-type")
+    const documentIdInput = document.getElementById("document-id")
 
-  function getFinalRotationPoint(selectedArea) {
-    const minAngle = selectedArea[0];
-    const maxAngle = selectedArea[1];
-    const possibleAngles = [];
-    for (let angle = minAngle; angle <= maxAngle; angle += 5) {
-      possibleAngles.push(angle);
+    documentTypeSelect.addEventListener("change", () => {
+      const selectedDocumentType = documentTypeSelect.value
+
+      if (selectedDocumentType === "cc" || selectedDocumentType === "ce") {
+        documentIdInput.setAttribute("inputmode", "numeric")
+        documentIdInput.setAttribute("pattern", "[0-9]*")
+        documentIdInput.placeholder = "Ingrese solo números"
+      } else if (selectedDocumentType === "pa") {
+        documentIdInput.removeAttribute("inputmode")
+        documentIdInput.removeAttribute("pattern")
+        documentIdInput.placeholder = "Ingrese letras y números"
+      } else {
+        documentIdInput.removeAttribute("inputmode")
+        documentIdInput.removeAttribute("pattern")
+        documentIdInput.placeholder = "Ingrese su documento"
+      }
+
+      // Validate document ID
+      const documentId = documentIdInput.value
+      const isValid = validateDocumentId(selectedDocumentType, documentId)
+
+      if (!isValid) {
+        const errorMessageSpan = document.getElementById("document-id-error");
+        errorMessageSpan.textContent = documentValidationPatterns[selectedDocumentType].errorMessage;
+      } else {
+        const errorMessageSpan = document.getElementById("document-id-error");
+        errorMessageSpan.textContent = "";
+      }
+    });
+  }
+
+  setupRegisterForm()
+
+  function validateDocumentId(documentType, documentId) {
+    const validationPattern = documentValidationPatterns[documentType];
+    if (!validationPattern) {
+      return true; // No validation needed
     }
-
-    const randomIndex = Math.floor(Math.random() * possibleAngles.length);
-    const finalRotationPoint = possibleAngles[randomIndex];
-    return finalRotationPoint;
+    const regex = new RegExp(validationPattern.pattern);
+    return regex.test(documentId);
   }
 
-  function displayModal(sector) {
-    messagePrizeWheel(sector);
-    if (sector.type === "joker") {
-      spinButton.disabled = false;
-      toggleModal(false);
-    } else {
-      toggleModal(true);
-    }
-  }
-
-  function toggleModal(show) {
-    if (show) {
-      modal.classList.remove("hidden");
-    } else {
-      modal.classList.add("hidden");
-    }
-  }
-
-  function messagePrizeWheel(sector) {
-    if (sector.type === "prize") {
-      console.log("spinPrizeWheel - type: prize, label:", sector.label);
-      const prizeText = `¡Felicitaciones, ganaste ${sector.label}!`;
-      modalText.textContent = prizeText;
-      finalizarButton.style.display = "block";
-    } else if (sector.type === "loss") {
-      console.log("spinPrizeWheel - type: loss, label:", sector.label);
-      modalText.textContent = "¡Pierdes! Inténtalo de nuevo.";
-      finalizarButton.style.display = "block";
-    } else {
-      modalText.textContent = "";
-      finalizarButton.style.display = "none";
-    }
-  }
-
-  function showPaymentSection() {
-    document.getElementById("game").classList.add("hidden");
-    document.getElementById("payment").classList.remove("hidden");
-
-    setTimeout(() => {
-      finalizarButton.style.display = "block";
-    }, 2000);
-  }
+  const registerForm = document.getElementById("register-form");
+  registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Add your form submission logic here
+    console.log("Form submitted (but not actually sending data)");
+  });
 })
